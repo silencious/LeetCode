e74b8efcba58fa1cca4a322f061734f0b305a5dc
Silencious <tjh0473@gmail.com>
2017-08-24 13:50:51 -0700

    654

--- 654.cpp
+++ 654.cpp
@@ -6,7 +6,7 @@ struct TreeNode {
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-class Solution {
+class MaximumBinaryTree {
 public:
 	TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
 		TreeNode* root = new TreeNode(nums[0]);
@@ -33,5 +33,23 @@ public:
 			}
 		}
 		return root;
+	}
+};
+class MaximumBinaryTree2 {
+public:
+	TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+		vector<TreeNode*> stack;
+		for(int i=0;i<nums.size();i++){
+			TreeNode* cur=new TreeNode(nums[i]);
+			while(!stack.empty()&&stack.back()->val<nums[i]){
+				cur->left=stack.back();
+				stack.pop_back();
+			}
+			if(!stack.empty()){
+				stack.back()->right=cur;
+			}
+			stack.push_back(cur);
+		}
+		return stack.front();
 	}
 };
