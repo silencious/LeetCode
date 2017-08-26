f9feeaad3180a0a992b116d8e4f38eb3677f346f
Silencious <tjh0473@gmail.com>
2017-08-25 22:22:59 -0700

    657

---
+++ 657.cpp
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+class JudgeRouteCircle {
+public:
+	bool judgeCircle(string moves) {
+		int x=0,y=0;
+		for(char ch:moves){
+			switch(ch){
+				case 'R': x++; break;
+				case 'L': x--; break;
+				case 'U': y++; break;
+				case 'D': y--; break;
+			}
+		}
+		return x==0&&y==0;
+	}
+};
